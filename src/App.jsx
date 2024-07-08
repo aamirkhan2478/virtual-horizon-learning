ff6a399aa87cc415df69d028090114dcfbd8c258
Darken-Tech <aamir@darkentech.com>
2024-07-08 09:41:42 +0500

    Setup tailwind and shadcn

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <p className="text-red-500">App</p>
+      <Button>Test Button</Button>
+    </>
+  );
+};
+
+export default App;
