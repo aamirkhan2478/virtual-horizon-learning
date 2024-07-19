fffc9d6e8d93f20e2018adf6fba00e61d32e0276
Numan Anwar <86041286+numananwar789@users.noreply.github.com>
2024-07-19 11:03:10 +0500

    New changes in auth pages

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -7,6 +7,23 @@ const loginUser = (values) => axios.post("/user/login", values);
 const forgotPassword = (values) => axios.post("/user/forgot-password", values);
 const resendEmail = (values) => axios.post("/user/resend-email", values);
 const resetPassword = (values) => axios.post("/user/reset-password", values);
+const updateUser = (values) => {
+    const token = localStorage.getItem("token");
+    return axios.put("/user/update-user", values, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+};
+const updatePassword = (values) => {
+    const token = localStorage.getItem("token");
+    return axios.put("/user/change-password", values, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+};
+
 
 export const useRegister = (onSuccess, onError) => {
     return useMutation(registerUser, {
@@ -45,6 +62,20 @@ export const useResendEmail = (onSuccess, onError) => {
 
 export const useResetPassword = (onSuccess, onError) => {
     return useMutation(resetPassword, {
+        onSuccess,
+        onError,
+    });
+};
+
+export const useUpdate = (onSuccess, onError) => {
+    return useMutation(updateUser, {
+        onSuccess,
+        onError,
+    });
+};
+
+export const useChangePassword = (onSuccess, onError) => {
+    return useMutation(updatePassword, {
         onSuccess,
         onError,
     });
