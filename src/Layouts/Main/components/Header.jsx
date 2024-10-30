ecd7f7e05306e6f550659e49a919a686685b86e0
Numan Anwar <numananwar789@gmail.com>
2024-10-30 09:35:14 +0500

    Logo changed

--- src/Layouts/Main/components/Header.jsx
+++ src/Layouts/Main/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import LogoImg from "../assets/images/vhl_logo.jpg";
+// import LogoImg from "../assets/images/vhl_logo.jpg";
+import LogoImg from "../assets/images/VHL_logo.png";
 import { LinkData } from "../assets/data/dummydata";
 import { NavLink, useNavigate } from "react-router-dom";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
